c56ee8fe2f5a314265631daa26da2cb87b9f5df0
Mattie Hibbs <mhibbs@email.sc.edu>
2021-06-11 14:10:02 -0400

    creates dataframe with three fields: contact name, contact email, and submission id: study name to be used as a template for a mail merge

--- Contact Info.R
+++ Contact Info.R
@@ -5,7 +5,7 @@ library(dplyr)
 #https://portal.nersc.gov/GEM/genomes/genome_metadata.tsv more comments on this file etc.
 gem_df <- read.csv("gem_data.csv", na = c("", "NA", "NULL"))
 #IMG datafile (insert url)
-img_df <- read.csv("imgdata.csv")
+img_df <- read.csv("img_new_data.csv", na = c("", "NA","NULL"))
 img_df <- img_df %>% rename(c("metagenome_id" = "IMG.Genome.ID"))
 # integrate metadata into each genome
 gems_joined <- gem_df %>%
@@ -26,7 +26,6 @@ sc_summary[ , "Percentage"] = sc_summary[ , "Percentage"]*100
 sc_summary_sorted <- sc_summary[order(sc_summary$Percentage, decreasing = TRUE),]
 
 sc_with_contact <- as.data.frame(table(gems_with_contact$`Sequencing.Center`))
-
 sc_wc_joined <- sc_with_contact %>%
   left_join(sc_summary, by = c("Var1"))  %>%
   rename(c("Sequencing Center" = "Var1", 
@@ -35,52 +34,18 @@ sc_wc_joined <- sc_with_contact %>%
            "Freq_w_Contact" = "Freq.x"))
 sc_wc_joined$Percent_w_Contact <- (sc_wc_joined$Freq_w_Contact/sc_wc_joined$Total_Frequency)*100
 
-cont_list <- as.data.frame(gems_with_contact[ ,c("metagenome_id", "Contact.Email", "Contact.Name", "Study.Name")])
-cont_list <- cont_list[!duplicated(cont_list$metagenome_id), ]
+cont_list <- as.data.frame(gems_with_contact[ ,c("Contact.Name", "Contact.Email","IMG.Submission.ID", "Study.Name")])
+cont_list <- cont_list[!duplicated(cont_list$IMG.Submission.ID), ]
+t <- split(cont_list, as.factor(cont_list$Contact.Name))
+tl <- lapply(t, function(x) within(x, C <- paste(IMG.Submission.ID, Study.Name, sep=':')))
+tl2 <- lapply(tl, function(x) aggregate(C~Contact.Name, x, paste, collapse="\n"))
+tl3 <- lapply(tl2, function(x) x[ , "C"])
+proj_info <- data.frame(matrix(unlist(tl3), nrow=length(tl3), byrow=T))
+cont_list3 <- cbind(cont_list2, proj_info)
+colnames(cont_list3) <- c("contact_name", "contact_email", "proj_id:study_name")
+cont_list3 <- cont_list3[order(cont_list3$contact_name),] 
+#write.xlsx(cont_list3, file = "permission_email_information.xlsx")
+#write.csv(cont_list3, file = "permission_email_information.csv")
+#write.table(cont_list3, "permission_email_information.tsv",quote=FALSE,sep='\t') )
+#cont_list <- cont_list[order(cont_list$Contact.Name),] 
 
-cl <- split(cont_list, as.factor(cont_list$Contact.Name))
-lev <- levels(as.factor(cont_list$Contact_Name))
-message <- c("Dear Dr. X,
-I hope this email finds you well. I am reaching out to seek permission to use data for which you are listed as PI in JGI's database. The data will be used by the Steen Lab, a research group at the University of Tenessee Knoxville in the Department of Microbiology. It will be used in DIAMOND searches for the bioinformatics portion of the project. 
-According to our records, you contributed the following data sets:")
-message <- rep(message, length(cl))
-
-y <- lapply(cl, function(x) x[ , "metagenome_id"])
-z <- lapply(cl, function(x) x[ , "Study.Name"])
-
-try <- list()
-for(i in 1:length(lev)) 
-   { 
-       a <- gsub("X", lev[i], message[i])
-      for(j in 1:nrow(as.data.frame(cont_list_list[i])))
-       {
-          s <- y[[i]][[j]]
-          t <- z[[i]][[j]]
-          st <- paste(s, ":", t)
-          a <- paste(a, st, sep = "\n")
-        }
-    try[i] <- a
-}
-
-
-closing = paste("Sincerely," , "The Steen Lab", sep = "\n")
-try2 <- list()
-for(i in 1:length(try))
-{
-  t <- paste(try[i], closing, sep = "\n")
-  try2[i] <- t
-}
-
-df <- as.data.frame(try2)
-dft <- t(df)
-nnames <- seq(1:266)
-#next, merge cont_list with email based on name? 
-row.names(dft) <- nnames
-
-cont_list3 <- cont_list[order(cont_list$Contact.Name),] 
-names2 <- seq(1:7254)
-row.names(cont_list3) <- c(names2)
-head(which(grepl("A", cont_list$Contact.Name)))
-cont_list3 <- cont_list3[-c(1:2069), ]
-contact_joined <- merge(x = cont_list3, y = cont_list2, by = "Contact.Name", all.x = TRUE)
-ce <- contact_joined[duplicated(contact_joined$Email_Text), 5]<- NA
