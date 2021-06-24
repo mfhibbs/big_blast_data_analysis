c06e461754cd401c79489a9b9a1e3f5a7ce1fec6
Mattie Hibbs <mhibbs@email.sc.edu>
2021-06-24 14:12:50 -0400

    Some code to analyize bit scores of .diamond resuls

---
+++ Bit Score Distribution Analysis.R
@@ -0,0 +1,47 @@
+#####
+# Bit Score Distribution Analysis
+#####
+
+#Read in .diamond files 
+setwd("/srv/data/big_blast/data/trial_diamond_results")
+ file_list <- list.files(path = "/srv/data/big_blast/data/trial_diamond_results")
+ dataset <- list()
+ for (i in 1:length(file_list)){
+    temp_data <- data.table::fread(file_list[i], header = F)
+    dataset[[i]] <- temp_data
+ }
+
+ #name columns in each df in list
+ headers <- c("Query Id", "Query Length", "Subject Length", "Perc. of Id. Matches", "Eval", "Bit Score", "Raw Score")
+ bit_score <- lapply(dataset, setNames, headers)
+ bit_score2 <- lapply(bit_score, function(x) x[ , c(1,6)])
+ 
+ #rbind all tables in list to create one dataframe
+ library(data.table)
+ bit_score_df <- rbindlist(bit_score2)
+
+ #method 1: sample a few lines from each mag 
+ bit_score_samp <- lapply(bit_scores2, function(x) x[sample(nrow(x), 3), ])
+ b_s_samp_df <- rbindlist(bit_scores_samp)
+ 
+ #method 2: sample a total of 10,000 mags from entire dataframe
+ bit_score_samp <-  bit_score_df[sample(nrow(bit_score_df), 10000), ]
+ 
+ #For both methods, plot the density distribution 
+ dens <- density(bit_score_df$'Bit Score')
+ plot(dens, frame = FALSE, col = "steelblue", 
+       main = "Density plot of bit scores", xlim = c(0, 1000)) 
+ 
+ #method 3: Quantile analysis
+ bit_scores <- bit_score_df$'Bit Score'
+ bit_scores <- bit_scores[order(-bit_scores)]
+ xq <- quantile(bit_scores) 
+ sapply(xq, function(y)which.min(abs(bit_scores - y))) 
+ q1 <- bit_scores[84596747:112798033]
+ q2 <- bit_scores[56250552:84596746]
+ q3 <- bit_scores[28102434 :56250551]
+ 
+ mags_tax <- read.csv("/home/mhibbs1/big_blast_data_analysis/mags_phy.csv")
+ mags_tax <- mags_tax[ , c("metagenome_id", "Phylum", "Class", "Order", "Family", "Genus", "Species")]
+ gems_joined_tax <- gems_joined %>%
+        left_join(mags_tax, by = c("metagenome_id"))
