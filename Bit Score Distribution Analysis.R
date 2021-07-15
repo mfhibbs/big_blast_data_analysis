f783e26c93c5416a57256bfb83ed878ef1400b5a
Mattie Hibbs <mhibbs@email.sc.edu>
2021-07-15 12:48:27 -0400

    edited code for getting samples of mags, bacteria, and archaea diamond results, as well as plotting their respective distributions of bit scores

--- Bit Score Distribution Analysis.R
+++ Bit Score Distribution Analysis.R
@@ -2,15 +2,57 @@
 # Bit Score Distribution Analysis
 #####
 
-#Read in .diamond files 
+#Reading in .diamond files:
+#The setwd function sets the working directory to wherever we have the diamond results saved
 setwd("/srv/data/big_blast/data/diamond_results")
- file_list <- list.files(path = "/srv/data/big_blast/data/diamond_results")
- dataset <- list()
- for (i in 1:length(file_list)){
-    temp_data <- data.table::fread(file_list[i], header = F)
-    dataset[[i]] <- temp_data
+#Then we put all of those files into a list called file_list
+file_list <- list.files(path = "/srv/data/big_blast/data/diamond_results")
+#You can check out the list by doing:
+head(file_list) #shows you first few files
+length(file_list) #shows you how many files are in the list
+
+#Now we use this loop to read in all of those files at the same time
+#First, create an empty list called dataset 
+mag_results <- list()
+r <- file_list[sample(length(file_list), 1000)]
+
+#Then, we loop through the file_list and read in each file, 
+#and then store each one in a spot in the list
+ for (i in 1:length(r)){
+    temp_data <- data.table::fread(r[i], header = F)
+    mag_results[[i]] <- temp_data
  }
 
+
+setwd("/srv/data/big_blast/data/isolate_results/bacteria")
+file_list <- list.files(path = "/srv/data/big_blast/data/isolate_results/bacteria")
+
+head(file_list) 
+length(file_list) 
+
+iso_bacteria <- list()
+r <- file_list[sample(length(file_list), 1000)]
+
+for (i in 1:length(r)){
+    temp_data <- data.table::fread(r[i], header = F)
+    iso_bacteria[[i]] <- temp_data
+}
+
+setwd("/srv/data/big_blast/data/isolate_results/archaea")
+file_list <- list.files(path = "/srv/data/big_blast/data/isolate_results/archaea")
+
+head(file_list) 
+length(file_list) 
+
+iso_archaea <- list()
+
+for (i in 1:length(file_list)){
+    temp_data <- data.table::fread(file_list[i], header = F)
+    iso_archaea[[i]] <- temp_data
+}
+
+zeros <- which(grepl("0", cols))
+isolates <- isolates[-c(zeros)]
  #name columns in each df in list
  headers <- c("Query Id", "Query Length", 
               "Subject Id", "Subject Length", 
@@ -20,35 +62,31 @@ setwd("/srv/data/big_blast/data/diamond_results")
               "Start of Alignment in query", "End of Alignment in query",
               "start of Alignment in subject", "End of Alignment in subject",
                "Eval", "Bit Score")
- bit_score <- lapply(dataset, setNames, headers)
- #bit_score <- lapply(bit_score, function(x) x[ , c(1,6)])
- 
- #rbind all tables in list to create one dataframe
- library(data.table)
- bit_score_df <- rbindlist(bit_score)
+mag_results <- lapply(mag_results, setNames, headers)
+iso_bacteria <- lapply(iso_bacteria, setNames, headers)
+iso_archaea <- lapply(iso_archaea, setNames, headers)
 
- #method 1: sample a few lines from each mag 
- bit_score_samp <- lapply(bit_score, function(x) x[sample(nrow(x), 3), ])
- b_s_samp_df <- rbindlist(bit_scores_samp)
+mag_results_df <- rbindlist(mag_results)
+iso_bact_df <- rbindlist(iso_bacteria)
+iso_arch_df <- rbindlist(iso_archaea)
+
+sequence <- seq(1:nrow(mag_results_df))
+r2_mag <- sample(sequence, 1000)
+sequence2 <- seq(1:nrow(iso_bact_df))
+r2_iso <- sample(sequence2, 1000)
+sequence3 <- seq(1:nrow(iso_arch_df))
+r3_iso <- sample(sequence3, 1000)
+
+mag_sample <- mag_results_df[r2_mag, ]
+iso_bact_sample <- iso_bact_df[r2_iso, ]
+iso_arch_sample <- iso_bact_df[r3_iso, ]
+
+dens_mags <- density(mag_sample$'Bit Score')
+dens_bact <- density(iso_bact_sample$'Bit Score')
+dens_arch <- density(iso_arch_sample$'Bit Score')
+plot(dens_mags, main = "Density of Bit Scores per Dataset")
+lines(dens_bact, col = "blue")
+lines(dens_arch, col = "red")
+
+
  
- #method 2: sample a total of 10,000 mags from entire dataframe
- bit_score_samp <-  bit_score_df[sample(nrow(bit_score_df), 10000), ]
- 
- #For both methods, plot the density distribution 
- dens <- density(b_s_samp_df$'Bit Score')
- plot(dens, frame = FALSE, col = "steelblue", 
-       main = "Density plot of bit scores", xlim = c(0, 1000)) 
- 
- #method 3: Quantile analysis
- bit_scores <- bit_score_df$'Bit Score'
- bit_scores <- bit_scores[order(-bit_scores)]
- xq <- quantile(bit_scores) 
- sapply(xq, function(y)which.min(abs(bit_scores - y))) 
- q1 <- bit_scores[84596747:112798033]
- q2 <- bit_scores[56250552:84596746]
- q3 <- bit_scores[28102434 :56250551]
- 
- mags_tax <- read.csv("/home/mhibbs1/big_blast_data_analysis/mags_phy.csv")
- mags_tax <- mags_tax[ , c("metagenome_id", "Phylum", "Class", "Order", "Family", "Genus", "Species")]
- gems_joined_tax <- gems_joined %>%
-        left_join(mags_tax, by = c("metagenome_id"))
